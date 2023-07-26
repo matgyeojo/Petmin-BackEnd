f9f386ec4d9f51fda82ab39182594e157e679fc5
e-7281998 <jejjcs43@naver.com>
2023-07-26 14:30:48 +0900

    first commit

---
+++ src/main/java/org/matgyeojo/PetminBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.matgyeojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetminBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetminBackEndApplication.class, args);
+	}
+
+}
