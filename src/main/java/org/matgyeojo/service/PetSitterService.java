49de06249cf198db70fd3e2b711fe22e8c7fe560
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-08-07 17:46:57 +0900

    feat : 돌봄 예약 진행중

--- src/main/java/org/matgyeojo/service/PetSitterService.java
+++ src/main/java/org/matgyeojo/service/PetSitterService.java
@@ -20,8 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.java.Log;
 
-
-
 @Service
 @Log
 public class PetSitterService {
@@ -64,13 +62,14 @@ public class PetSitterService {
 	}
 
 	// 펫시터 업데이트
-	public String petsitterUpdate(String userId, MultipartFile[] sitterHouse, String sitterHousetype, String sitterMsg) throws IOException {
+	public String petsitterUpdate(String userId, MultipartFile[] sitterHouse, String sitterHousetype, String sitterMsg)
+			throws IOException {
 		Users user = userrepo.findById(userId).orElse(null);
 
-		//이미지 리스트시작
+		// 이미지 리스트시작
 		String image_list = "[";
 
-		//배열로 된 파일목록 하나씩 짤라서 저장
+		// 배열로 된 파일목록 하나씩 짤라서 저장
 		for (MultipartFile img : sitterHouse) {
 			if (img != null && !img.isEmpty()) {
 				String sotredFileName = s3uploader.upload(img, "house");
@@ -119,57 +118,77 @@ public class PetSitterService {
 		System.out.println(msg);
 		return msg;
 	}
-	
-	//펫시터 스케쥴 저장
+
+	// 펫시터 스케쥴 저장
 	public int petsitterScadure(String sitterId, String scheduleDay, String[] scheduleHour, String dolbomOption) {
-		int msg =0;
+		int msg = 0;
 		Users sitter = userrepo.findById(sitterId).orElse(null);
-		//가능한 시간을 배열로 입력받아 따로따로 저장
-		for(String s:scheduleHour) {
-			if(dolbomrepo.findByUser2AndScheduleDayAndScheduleHour(sitterId,scheduleDay, s)>0) {
-				continue;
-			} 
+
+		// 그러면 처음 만드는 거 이거나 있는데 값이 안들어 온 것.
+		// 그러면 삭제
+		// 그러면 option값만 변경되며능ㄴ?
+		for (String s : scheduleHour) {
+			List<Dolbom> dols = dolbomrepo.findByUser2AndScheduleDay(sitter, scheduleDay);
 			
-			Dolbom dol = Dolbom.builder().user2(sitter).scheduleDay(scheduleDay).scheduleHour(s).dolbomStatus(false).dolbomOption(dolbomOption).build();
+			for (Dolbom dolbom : dols) {
+				//리스트에서 모든 애들중에 돌봄예약 안된애들은 그냥 삭제
+				if ( dolbom.getDolbomStatus().equals(false)) {
+					dolbomrepo.delete(dolbom);
+				}
+				//삭제 안된 애들중에 돌봄 옵션이 일치하지 않으면
+				if (!dolbom.getDolbomOption().equals(dolbomOption)&& dolbom.getDolbomStatus().equals(false)) {
+					dolbom.setDolbomOption(dolbomOption);
+					dolbomrepo.save(dolbom);
+				}
+			}			
+		}
+
+		// 가능한 시간을 배열로 입력받아 따로따로 저장
+		for (String s : scheduleHour) {
+			if (dolbomrepo.findByUser2AndScheduleDayAndScheduleHour(sitterId, scheduleDay, s) > 0) {
+				continue;
+			}
+
+			Dolbom dol = Dolbom.builder().user2(sitter).scheduleDay(scheduleDay).scheduleHour(s).dolbomStatus(false)
+					.dolbomOption(dolbomOption).build();
 			dolbomrepo.save(dol);
 		}
 		List<Dolbom> dols = dolbomrepo.findByScheduleDay(scheduleDay);
-		int count=0;
-		for(Dolbom dol : dols) {
+		int count = 0;
+		for (Dolbom dol : dols) {
 			count++;
 		}
-		if(count>0) {
-			msg=1;
-		} 
-		
+		if (count > 0) {
+			msg = 1;
+		}
+ 
 		return msg;
 	}
-	
-	//펫시터 프로필 가져오기
+
+	// 펫시터 프로필 가져오기
 	public PetsitterProfile getSitter(String userId) {
 		PetsitterProfile sitter = petsitterrepo.findById(userId).orElse(null);
 		return sitter;
 	}
-	
-	//펫시터 일정 가져오기
-	public List<Object> getSchedure(String sitterId,String scheduleDay){
+
+	// 펫시터 일정 가져오기
+	public List<Object> getSchedure(String sitterId, String scheduleDay) {
 		Users user = userrepo.findById(sitterId).orElse(null);
-		List<Dolbom> dols = dolbomrepo.findByUser2AndScheduleDay(user,scheduleDay);
+		List<Dolbom> dols = dolbomrepo.findByUser2AndScheduleDay(user, scheduleDay);
 		List<Object> result = new ArrayList<>();
-		
-		
-		for(Dolbom dol:dols) {
+
+		for (Dolbom dol : dols) {
 			HashMap<String, Object> map = new HashMap<>();
 			map.put("day", scheduleDay);
 			map.put("dolbomOption", dol.getDolbomOption());
-			
+
 			HashMap<String, Object> map2 = new HashMap<>();
 			map2.put("Hour2", dol.getScheduleHour());
 			map2.put("dolbomStatus", dol.getDolbomStatus());
 			map.put("Hour", map2);
 			result.add(map);
 		}
-		
+
 		return result;
 	}
 
