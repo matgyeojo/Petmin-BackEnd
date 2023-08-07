3b85255f3d2a265fcaa4c99ebbd9dba9a0a76451
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-08-07 10:46:12 +0900

    feat : 펫시터 프로필 가져오기 및 스케쥴 등록 수정

--- src/main/java/org/matgyeojo/repository/DolbomRepo.java
+++ src/main/java/org/matgyeojo/repository/DolbomRepo.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 import org.matgyeojo.dto.Dolbom;
 import org.matgyeojo.dto.Users;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DolbomRepo extends CrudRepository<Dolbom, Integer>{
 
 	//펫시터 가능한 날짜 필터링
 	public List<Dolbom> findByScheduleDay(String scheduleDay);
+	//펫시터 가능한 날짜 중복체크 
+	@Query(value = "select count(*) from dolbom d where d.petsitter_id = ?1 and d.schedule_day = ?2 and d.schedule_hour = ?3", nativeQuery = true)
+	public int findByUser2AndScheduleDayAndScheduleHour(String SitterId,String scheduleDay,String scheduleHour);
+	
 	//펫시터 돌봄옵션 필터링
 	public List<Dolbom> findByDolbomOptionLike(String dolbomOption);
 	//펫시터가 누군지 필터링
 	public List<Dolbom> findByUser2(Users user);
 }
+ 
