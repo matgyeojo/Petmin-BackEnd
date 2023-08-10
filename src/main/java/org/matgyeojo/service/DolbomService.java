87f949566041906fd571719978d452b03e4e324b
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-08-10 10:36:07 +0900

    dolbom filter

--- src/main/java/org/matgyeojo/service/DolbomService.java
+++ src/main/java/org/matgyeojo/service/DolbomService.java
@@ -52,139 +52,87 @@ public class DolbomService {
 		if (userAddress.equals("")) {// 입력값에 아무것도 입력 안하면 기본으로 자신의 주소
 			userAddress = u.getUserAddress();
 		}
-		
+
 		List<String> filter4 = new ArrayList<>();
-		if(petWeight.equals("소형견")) {
+		if (petWeight.equals("소형견")) {
 			int petweight = 10;
-		filter4 = petrepo.findso(userAge, userSex, userAddress, sitterHousetype, petSex, petweight);
-		}else if(petWeight.equals("중형견")) {
+			filter4 = petrepo.findso(userAge, userSex, userAddress, sitterHousetype, petSex, petweight);
+		} else if (petWeight.equals("중형견")) {
 			int petweight = 25;
-			filter4 = petrepo.findjoong(userAge, userSex, userAddress, sitterHousetype, petSex, petweight);		
-		}else {
+			filter4 = petrepo.findjoong(userAge, userSex, userAddress, sitterHousetype, petSex, petweight);
+		} else {
 			int petweight = 25;
 			filter4 = petrepo.findsdae(userAge, userSex, userAddress, sitterHousetype, petSex, petweight);
 		}
-		
-		
-		
-		
+
 		// 1.users 테이블 = 성별, 나이
 		// 2.sitter테이블 = 집 타입
 		// 3.펫 프로필 테이블 = 성별 , 몸무게 (소형견 : 10kg미만,중형견:10~25,대형견 25~)
 		// 성별,나이,사는곳
 
-
 		// List에 map을 저장
 		List<Object> result = new ArrayList<>();
 
-		List<String> options = new ArrayList<>();
-		List<String> days = new ArrayList<>();
+	
+		HashMap<String, String> success = new HashMap<String, String>();
 
 		// 만약 필터에 걸리는게 하나도 없으면
 		if (filter4.isEmpty()) {
 			// 유저 20개
 			List<String> userss = userrepo.findUserAll();
-			
+			success.put("추천", "실패");
+			result.add(success);
 			for (String use : userss) {
 				HashMap<String, Object> map = new HashMap<>();
 				Users user = userrepo.findById(use).orElse(null);
 				PetsitterProfile sitter = petsitterrepo.findById(use).orElse(null);
-	
-
-				if (sitter != null) {
-					// 프론트에서 필터링하는데 필요한 부분
-					List<Dolbom> dol = dolbomrepo.findByUser2(user);
-
-					for (Dolbom d : dol) {
-						int count1 = 0;// 옵션 일치하는지
-						int count2 = 0;
-						for (String op : options) {// 옵션 가져오기
-							if (d.getDolbomOption().equals(op)) {
-								count1++;
-							}
-						}
-						if (count1 == 0) {// 일치하는게 없으면
-							options.add(d.getDolbomOption());
-
-						} else {
-							count1 = 0;
-						}
-
-						for (String da : days) {// 날짜 가져오기
-							if (d.getScheduleDay().equals(da)) {
-								count2++;
-							}
-						}
-						if (count2 == 0) {// 일치하는게 없으면 추가
-							days.add(d.getScheduleDay());
-						} else {
-							count2 = 0;
-						}
-					}
-
-					// 리스트에 필요한 정보 추가.
-					map.put("userName", user.getUserName());
-					map.put("userAddress", user.getUserAddress());
-					if (sitter.getSitterHouse() == null) {
-						map.put("sitterHouse", "이미지 없음");
-					} else {
-						map.put("sitterHouse", sitter.getSitterHouse());
-					}
-					map.put("sitterMsg", sitter.getSitterMsg());
-					map.put("sitterTem", sitter.getSitterTem());
-					map.put("scheduleDay", days);
-					map.put("dolbomOption", options);
-					result.add(map);
-				}
-			}
-			return result;
-		} else {
-			for (String fil : filter4) {
-				// 필요한 정보들만 추출
-				HashMap<String, Object> map = new HashMap<>();
-				Users user = userrepo.findById(fil).orElse(null);
-				PetsitterProfile sitter = petsitterrepo.findById(fil).orElse(null);
-
+				HashMap<String, Object> schopmap = new HashMap<String, Object>();
+				
 				if (sitter != null) {
 
 					// 프론트에서 필터링하는데 필요한 부분
 					List<Dolbom> dol = dolbomrepo.findByUser2(user);
-
+				
 					for (Dolbom d : dol) {
-						int count1 = 0;// 옵션 일치하는지
-						int count2 = 0;
-						for (String op : options) {// 옵션 가져오기
-							if (d.getDolbomOption().equals(op)) {
-								count1++;
-							}
-						}
-						if (count1 == 0) {// 일치하는게 없으면
-							options.add(d.getDolbomOption());
-
-						} else {
-							count1 = 0;
-						}
-
-						for (String da : days) {// 날짜 가져오기
-							if (d.getScheduleDay().equals(da)) {
-								count2++;
-							}
-						}
-						if (count2 == 0) {// 일치하는게 없으면 추가
-							days.add(d.getScheduleDay());
-						} else {
-							count2 = 0;
-						}
+						schopmap.put(d.getScheduleDay(), d.getDolbomOption());
+					
 					}
-
 					map.put("userName", user.getUserName());
 					map.put("userAddress", user.getUserAddress());
 					map.put("sitterHouse", sitter.getSitterHouse());
 					map.put("sitterMsg", sitter.getSitterMsg());
 					map.put("sitterTem", sitter.getSitterTem());
-					map.put("scheduleDay", days);
-					map.put("dolbomOption", options);
+					map.put("scheduleDay", schopmap);
 					result.add(map);
+				}
+			}
+			return result;
+		} else {
+			success.put("추천", "성공");
+			result.add(success);
+			for (String fil : filter4) {
+				// 필요한 정보들만 추출
+				HashMap<String, Object> map = new HashMap<>();
+				Users user = userrepo.findById(fil).orElse(null);
+				PetsitterProfile sitter = petsitterrepo.findById(fil).orElse(null);
+				HashMap<String, Object> schopmap = new HashMap<String, Object>();
+				
+				if (sitter != null) {
+					// 프론트에서 필터링하는데 필요한 부분
+					List<Dolbom> dol = dolbomrepo.findByUser2(user);
+		
+					
+					for (Dolbom d : dol) {
+						schopmap.put(d.getScheduleDay(), d.getDolbomOption());
+					}
+					map.put("userName", user.getUserName());
+					map.put("userAddress", user.getUserAddress());
+					map.put("sitterHouse", sitter.getSitterHouse());
+					map.put("sitterMsg", sitter.getSitterMsg());
+					map.put("sitterTem", sitter.getSitterTem());
+					map.put("scheduleDay", schopmap);
+					result.add(map);
+
 				}
 			}
 			return result;
@@ -246,7 +194,7 @@ public class DolbomService {
 		for (String s : scheduleHour) {// 스케쥴 시간 포문
 			// 돌봄테이블에서 펫시터의 날짜 시간 일치하는거 가져와서 예약되었다고 표시.
 			Dolbom dol = dolbomrepo.findByUser2AndScheduleDayAndScheduleHour(sitter, scheduleDay, s);
-			dol.setDolbomStatus(2); //0->안된거 1-> 된거 2->대기
+			dol.setDolbomStatus(2); // 0->안된거 1-> 된거 2->대기
 			dol.setPetProfile(pet);
 			dolbomrepo.save(dol);
 			msg = 1;
